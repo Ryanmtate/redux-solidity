9fd50c78974d5b3e56ead37ee1ec24573a338d10
Ryan Tate <ryan@ventureequity.co>
2016-10-26 17:27:51 -0700

    This is automated... read the logs for the changes

--- src/contractStateEngine.js
+++ src/contractStateEngine.js
@@ -147,8 +147,8 @@ export default class StateEngine {
   getTransactionReceipt(txHash, _counter) {
     return new Promise((resolve, reject) => {
       let counter = _counter || 0;
-      if (counter > 30 ) { reject(new Error('Could not find transaction receipt.')); }
-      Promise.delay(15000).then(() => {
+      if (counter > 200 ) { reject(new Error('Could not find transaction receipt.')); }
+      Promise.delay(500).then(() => {
         return this.eth.getTransactionReceiptAsync(txHash);
       }).then((txReceipt) => {
         if(!txReceipt){
